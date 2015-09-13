aa63fa80358e397e94053cfe053edf97676921b8
Lisa Wong <miss.lisa19@hotmail.com>
2015-09-12 17:05:14 -0700

    get all the posts when we load the home page

--- YWCA.js
+++ YWCA.js
@@ -1,19 +1,21 @@
 Posts = new Mongo.Collection("posts");
 
-if (Meteor.isClient)
-{
-
+if (Meteor.isClient) {
+    Template.body.helpers({
+        posts: function () {
+            return Posts.find({});
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
 
-Router.route('/', function ()
-{
-	this.render('home');
+Router.route('/', function () {
+    this.render('home');
 });
 
 Router.route('/post1');
@@ -21,24 +23,22 @@ Router.route('/post1');
 Router.route('/post2');
 
 // given a url like "/post/5"
-Router.route('/post/:_id', function ()
-{
-	var params = this.params; // { _id: "5" }
-	var id = params._id; // "5"
-	console.log(id);
+Router.route('/post/:_id', function () {
+    var params = this.params; // { _id: "5" }
+    var id = params._id; // "5"
+    console.log(id);
 });
 
-Router.route('/newpost/:_id/:_question/:_answer/:_picture', function ()
-{
-	var params = this.params; // { _id: "5" }
-	var id = params._id; // "5"
-	var question = params._question;
-	var pictureBase64 = params._picture;
-	var answer = params._answer;
-	Posts.insert({
-		id: id,
-		question: question,
-		picture: pictureBase64,
-		answer: answer
-	});
+Router.route('/newpost/:_id/:_question/:_answer/:_picture', function () {
+    var params = this.params; // { _id: "5" }
+    var id = params._id; // "5"
+    var question = params._question;
+    var pictureBase64 = params._picture;
+    var answer = params._answer;
+    Posts.insert({
+        id: id,
+        question: question,
+        picture: pictureBase64,
+        answer: answer
+    });
 });
